3c33f09f93c6ec27d48c5b3195c348da78b2dbdb
Metahaem <tom.rowson@gmail.com>
2019-01-21 16:13:11 +0000

    added users to routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :acts, only: [:index, :show, :create]
+      resources :users, only: [:index, :show, :create]
     end
   end
 end
