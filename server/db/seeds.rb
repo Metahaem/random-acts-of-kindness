c07eb1a51b6b9d89d2c6dd89c177eac36ff6d1da
Metahaem <tom.rowson@gmail.com>
2019-01-21 16:11:47 +0000

    added testing seed data

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{name: "Janine"}, {name: "Tom"}])
+
+categories = Category.create([{name: "Animals"}])
+
+# acts = [
+#   { content: "Pet a puppy", user: users[0].id, category: categories[0].id  }
+# ]
+#
+# acts.each do
+#   |act| Act.create(content: act.content, user: act.user, category: act.category )
+# end
+
+Act.create( content: "Pet a puppy", user: users[0], category: categories[0])
