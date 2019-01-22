20f553694cef05f82d003479edbf436950c3c4d3
ja9-look <janineluk95@gmail.com>
2019-01-22 14:03:15 +0000

    added image url to Acts model and updated seed data with gifs

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -27,16 +27,16 @@ categories = [
 categories.each {|category| Category.create!(category)}
 
 acts = [
-  {content: "Pet a puppy", user_id: 1, category_id: 1},
-  {content: "Pick up some litter", user_id: 2, category_id: 2},
-  {content: "Donate to a local charity", user_id: 1, category_id: 4},
-  {content: "Buy a hot meal for a homeless person", user_id: 2, category_id: 4},
-  {content: "Volunteer at an animal shelter", user_id: 1, category_id: 1},
-  {content: "Cook/bake something for your neighbour", user_id: 2, category_id: 3},
-  {content: "Send a nice message to someone you care about", user_id: 2, category_id: 3},
-  {content: "Compliment someone", user_id: 1, category_id: 3},
-  {content: "Invite a co-worker to lunch", user_id: 2, category_id: 5},
-  {content: "Plant a tree", user_id: 1, category_id: 2}
+  {content: "Pet a puppy", user_id: 1, category_id: 1, image_url: "http://gph.is/1fg8eOM"},
+  {content: "Pick up some litter", user_id: 2, category_id: 2, image_url: "https://gph.is/2GKJihr"},
+  {content: "Donate to a local charity", user_id: 1, category_id: 4, image_url: "https://gph.is/2behKo2"},
+  {content: "Buy a hot meal for a homeless person", user_id: 2, category_id: 4, image_url: "https://gph.is/2R6uG5Y"},
+  {content: "Volunteer at an animal shelter", user_id: 1, category_id: 1, image_url: "https://gph.is/2ot44sF"},
+  {content: "Cook/bake something for your neighbour", user_id: 2, category_id: 3, image_url: "https://gph.is/2vzSnXT"},
+  {content: "Send a nice message to someone you care about", user_id: 2, category_id: 3, image_url: "https://gph.is/2oZE9yd"},
+  {content: "Compliment someone", user_id: 1, category_id: 3, image_url: "https://gph.is/2mUvlpi"},
+  {content: "Invite a co-worker to lunch", user_id: 2, category_id: 5, image_url: "https://gph.is/1nRq4ci"},
+  {content: "Plant a tree", user_id: 1, category_id: 2, image_url: "https://gph.is/2bbWfBb"}
 ]
 
 
