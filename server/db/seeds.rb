a44092f98956d1489a0ae04e79fdf245c15a53c3
ja9-look <janineluk95@gmail.com>
2019-01-22 16:55:19 +0000

    updated seed data with the correct image urls

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -27,16 +27,16 @@ categories = [
 categories.each {|category| Category.create!(category)}
 
 acts = [
-  {content: "Pet a puppy", user_id: 1, category_id: 1, image_url: "http://gph.is/1fg8eOM"},
-  {content: "Pick up some litter", user_id: 2, category_id: 2, image_url: "https://gph.is/2GKJihr"},
-  {content: "Donate to a local charity", user_id: 1, category_id: 4, image_url: "https://gph.is/2behKo2"},
-  {content: "Buy a hot meal for a homeless person", user_id: 2, category_id: 4, image_url: "https://gph.is/2R6uG5Y"},
-  {content: "Volunteer at an animal shelter", user_id: 1, category_id: 1, image_url: "https://gph.is/2ot44sF"},
-  {content: "Cook/bake something for your neighbour", user_id: 2, category_id: 3, image_url: "https://gph.is/2vzSnXT"},
-  {content: "Send a nice message to someone you care about", user_id: 2, category_id: 3, image_url: "https://gph.is/2oZE9yd"},
-  {content: "Compliment someone", user_id: 1, category_id: 3, image_url: "https://gph.is/2mUvlpi"},
-  {content: "Invite a co-worker to lunch", user_id: 2, category_id: 5, image_url: "https://gph.is/1nRq4ci"},
-  {content: "Plant a tree", user_id: 1, category_id: 2, image_url: "https://gph.is/2bbWfBb"}
+  {content: "Pet a puppy", user_id: 1, category_id: 1, image_url: "https://media.giphy.com/media/qPuhFBQt8xLEY/giphy.gif"},
+  {content: "Pick up some litter", user_id: 2, category_id: 2, image_url: "https://media.giphy.com/media/1YctWh7oIsZPkTXMw2/giphy.gif"},
+  {content: "Donate to a local charity", user_id: 1, category_id: 4, image_url: "https://media.giphy.com/media/3o6Zt3OhbsQ5VLPmBW/giphy.gif"},
+  {content: "Buy a hot meal for a homeless person", user_id: 2, category_id: 4, image_url: "https://media.giphy.com/media/5S4CcNKWy7a28XDWyF/giphy.gif"},
+  {content: "Volunteer at an animal shelter", user_id: 1, category_id: 1, image_url: "https://media.giphy.com/media/3o7bueStqEca4YftOE/giphy.gif"},
+  {content: "Cook/bake something for your neighbour", user_id: 2, category_id: 3, image_url: "https://media.giphy.com/media/Y09s2Frxp7wpBGXTyt/giphy.gif"},
+  {content: "Send a nice message to someone you care about", user_id: 2, category_id: 3, image_url: "https://media.giphy.com/media/Vj02LjCFMdk0U/giphy.gif"},
+  {content: "Compliment someone", user_id: 1, category_id: 3, image_url: "https://media.giphy.com/media/3o751Sq3f0eeLjQB0Y/giphy.gif"},
+  {content: "Invite a co-worker to lunch", user_id: 2, category_id: 5, image_url: "https://media.giphy.com/media/yxVRIRF0WJUkBckw8g/giphy.gif"},
+  {content: "Plant a tree", user_id: 1, category_id: 2, image_url: "https://media.giphy.com/media/3o7TKp3cFUIxiCW8KI/giphy.gif"}
 ]
 
 
